96160cf83d039f318af3100d1a6a43076667a7b1
alibyatra <57876801+alibyatra@users.noreply.github.com>
2020-04-23 18:15:27 +0300

    Add files via upload

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <exception>
 #include <bits/stdc++.h>
+
 using namespace std;
  namespace family{
 
@@ -17,51 +18,195 @@ using namespace std;
 
  Tree::Tree(): root(NULL){}
 
-
  Tree::Tree(string name)
  {
-    this->root = new node(name);;
+    this->root = new node(name);
  }
 
+void Tree::findSon(node *root, node** ptr ,const string son) 
+ {
+    if(root == NULL) *ptr=NULL;
+    if(root->name == son)
+    {
+      *ptr = root  ;
+    }
+    else
+    {
+        Tree::findSon(root->left, ptr ,son );
+        Tree::findSon(root->right, ptr ,son );
+    }
+ }
 
  Tree& Tree::addFather(string son, string father)
  {
+    if (son=="" || father=="")
+    {
+    throw 'Error, there is at least one word empty!';
+    return *this;
+    }
+     if(this->root==NULL)
+    {
+        throw 'The family tree doesnt exist!';
+        return  *this;
+    }
+    node *ptr;
+    findSon(this->root,&ptr ,son);
+    if(ptr==NULL)
+    {
+        throw 'the son doesnt exist!';
+        return *this;
+    }
+    else
+    {
+        ptr->left=new node(father);
+    }
     return *this;
  }
  
-
-
  Tree& Tree::addMother(string son, string mother)
  {
+    if (son=="" || mother=="")
+    {
+    throw 'Error, there is at least one word empty!';
+    return *this;
+    }
+     if(this->root==NULL)
+    {
+        throw 'The family tree doesnt exist!';
+        return  *this;
+    }
+    node *ptr;
+    findSon(this->root,&ptr ,son);
+    if(ptr==NULL)
+    {
+        throw 'the son doesnt exist!';
+        return *this;
+    }
+    else
+    {
+        ptr->right=new node(mother);
+    }
     return *this;
  }
 
- 
-
- string Tree::relation(string name)
- {  
-    return "relation";    
- }
 
 
 
-string Tree::find(string a)
+
+
+ string Tree::relation(string relative)
  {
-    if (a.compare("uncle") == 0)
+    string relate="unrelated";
+    if (relative == "")
+    throw relate;
+    else
     {
-         return  string("the tree can't handle the uncle relation");
+       if (root->name == relative)
+       {
+       relate="me";
+       return relate;
+       }
+       if (root->left!=nullptr)
+       {
+          if (relate != "unrelated")
+          if (root->left->name==relative)
+          relate="father";
+           relate = left->relation(relative);
+          relate=relation(relative);
+       }
+       if (root->right->name==relative)
+       relate="mother";
+       relation(root->right->name);
     }
-    return "find";
+    
+    return relate;    
  }
 
+string Tree::find(string relation, node *root)
+{
+if(relation == "grandfather")
+    {
+        return root->left->left->name;
+    }
+    else if(relation == "grandmother")
+    {
+        if(root->left->right != nullptr)
+        {
+            return root->left->right->name;
+        }
+        if(root->right->right != nullptr)
+        {
+            return root->right->right->name;
+        }
+    }
+    else
+    {
+        string str ;
+        str.append(relation.begin()+6,relation.end());
+        return find(str, root->left);
+    }
+    throw "we couldn't find the "+ relation +"in the tree";
+}
+
+string Tree::find(string relation)
+{
+    if (relation == "me")
+        return (root->name);
+    if ((relation == "father") && (root->left!=nullptr))
+        return (root->left->name);
+    if ((relation == "mother") && (root->right!=nullptr))
+        return (root->right->name);
+        else 
+        {   
+           try
+            {
+              return find(relation, root);
+            }  
+            catch(const std::exception& e)
+            {
+               throw runtime_error("The tree cannot handle the: " + relation + " relation");
+            }
+         }
+}
+
+void printFamily(node *root, int space)  
+{   
+   int COUNT=10;
+    if (root == NULL)  
+        return;   
+    space += COUNT;  
+    printFamily(root->right, space);  
+    cout<<endl;  
+    for (int i = COUNT; i < space; i++)  
+        cout<<" ";  
+    cout<<root->name<<"\n";    
+    printFamily(root->left, space);  
+}  
 
  void Tree::display()
  {
-    cout << "display"<< endl; 
+    cout << "display Fmaily :\n"<< endl; 
+    printFamily(this->root,0);
+   cout << "**********************************" << endl; 
  }
 
-
- string Tree::remove(string name){
-     return "remove" ;
+ void Tree::remove(node* root)
+ {
+  if(root != nullptr)
+  {
+  remove(root->left);
+  remove(root->right);
+  delete root;
+  }
  }
-}
+
+ string Tree::remove(string name)
+ {
+    node *ptr;
+    findSon(this->root,&ptr,name);
+    if(ptr!=NULL)
+        remove(ptr);
+    else 
+        cout << "the Family Tree doesn't exist" << endl;
+  }
+ }
--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -3,39 +3,38 @@
 #include<stdlib.h>
 #include <iostream>
 #include <string.h>
-using namespace std;
 
+using namespace std;
 namespace family{
 
 class node
 {
 public:
+    int gender;
     string name;
     node *left; //Father
     node *right; //Mother
     node(string); 
 };
 
-
-class Tree {
-
-    public:
-
+class Tree 
+{
+public:
     node* root;
 
     Tree();
     Tree(string); 
-    
-    Tree& addFather(string , string); 
-    Tree& addMother(string, string); 
-
+    void findSon(node *root, node** ptr ,const string son);
+    Tree& addFather(string son , string father); 
+    Tree& addMother(string son, string mother); 
+    void printFamily(node *root, int space);
     void display();  
     
-    string relation(string); 
-    string find(string); 
-    
-    string remove(string node); 
-
+    string relation(string);
+    string find(string relation, node *root); 
+    string find(string relation); 
+    void remove(node* name);
+    string remove(string name);
 };
 }
 #endif
